fd2d1031b3fd72ce9a30f48bfc928bbe96c597cf
Md Zeeshan <imerzeeshan@gmail.com>
2025-01-10 15:22:23 +0530

    Subscribe To Us

---
+++ 02_JavaScriptMiniProjects/50-SubscribeToUs/script.js
@@ -0,0 +1,19 @@
+let nameEl = document.getElementById('name');
+let nameErrMsg = document.getElementById('nameErrMsg');
+let emailEl = document.getElementById('email');
+let emailErrMsg = document.getElementById('emailErrMsg');
+
+nameEl.addEventListener('blur', function(event) {
+    if (event.target.value === "") {
+        nameErrMsg.textContent = "Required*";
+    } else {
+        nameErrMsg.textContent = "";
+    }
+});
+emailEl.addEventListener('blur', function(event) {
+    if (event.target.value === "") {
+        emailErrMsg.textContent = "Required*";
+    } else {
+        emailErrMsg.textContent = "";
+    }
+});
